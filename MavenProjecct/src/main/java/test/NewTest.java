9864c52dcfdea0fe87cb1897a649b320cb1820fd
KrithiRatee <KrithiRatee@LAPTOP-AU8CQ95V.home>
2018-07-27 14:25:20 -0400

    Test1

---
+++ MavenProjecct/src/main/java/test/NewTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+ 
+public class NewTest {
+ 
+ public WebDriver driver;
+ 
+  @Test
+  public void openMyBlog() {
+ driver.get("https://www.softwaretestingmaterial.com/");
+//we expect the title “Google “ should be present 
+String Expectedtitle = "Software Testing Material";
+//it will fetch the actual title 
+String Actualtitle = driver.getTitle();
+System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+//it will compare actual title and expected title
+Assert.assertEquals(Actualtitle, Expectedtitle);
+//print out the result
+System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+   
+   System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\Drivers\\chromedriver.exe");
+   driver = new ChromeDriver();
+   
+  }
+ 
+  @AfterClass
+  public void afterClass() {
+   driver.quit();
+  }
+ 
+}
